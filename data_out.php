b610d79fb28905bef9b86aae8725c05aedbe8c82
peter279k <peter279k@gmail.com>
2015-11-28 13:19:38 +0800

    新增json data

---
+++ data_out.php
@@ -0,0 +1,10 @@
+<?php
+	//get data about beauty data
+	require 'libs/connectDB.php';
+	header("Content-Type: application/json; charset=utf-8");
+	$connection = new connectDB();
+	$conn = $connection -> initialDB();
+	$result = $connection -> processData($conn, $sql, $data, "get-data");
+	$connection -> connectClose();
+	echo json_decode($result);
+?>
