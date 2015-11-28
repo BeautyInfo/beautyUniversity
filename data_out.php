d5e52bb2b6c144d8e83b786b881adde52702b9e0
peter279k <peter279k@gmail.com>
2015-11-28 15:38:49 +0800

    add gz_handler

--- data_out.php
+++ data_out.php
@@ -2,6 +2,7 @@
 	//get data about beauty data
 	header("Content-Type: application/json; charset=utf-8");
 	header("Access-Control-Allow-Origin: *");
+	ob_start("ob_gzhandler");
 	require 'libs/connectDB.php';
 	$connection = new connectDB();
 	$conn = $connection -> initialDB();
