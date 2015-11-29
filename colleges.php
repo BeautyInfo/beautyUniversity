93272f0f247a83ededb12475232f9c4222138983
peter279k <peter279k@gmail.com>
2015-11-29 16:19:57 +0800

    using content-type  text/html

--- colleges.php
+++ colleges.php
@@ -1,6 +1,7 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
-	header("Content-Type: application/json; charset=utf-8");
+	//header("Content-Type: application/json; charset=utf-8");
+	header("Content-Type: text/html; charset=utf-8");
 	ob_start("ob_gzhandler");
 	require 'libs/LIB_http.php';
 	$handle = fopen("u1_new.csv", "r");
@@ -76,7 +77,7 @@
 			}
 		}
 
-		echo json_encode($colleges, JSON_PRETTY_PRINT);
+		//echo json_encode($colleges, JSON_PRETTY_PRINT);
 	}
 
 	function matchCollegeName($msg, $colleges)
