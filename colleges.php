85b58d516cd64895c7e4adde165fe49dc6e490c6
peter279k <peter279k@gmail.com>
2015-11-29 14:49:58 +0800

    add comment

--- colleges.php
+++ colleges.php
@@ -28,6 +28,7 @@
 		{
 			$message = str_replace("台", "臺", $data[$i]["message"]);
 			$message = str_replace("表特大學", "", $message);
+			/*
 			for($j=3$j<count($colleges);$j++)
 			{
 				$value = str_replace("國立", "", $colleges[$j]["name"]);
@@ -63,8 +64,8 @@
 						$colleges[$j]["count"] += 1;
 
 				}
-				*/
 			}
+			*/
 		}
 
 		echo json_encode($colleges);
