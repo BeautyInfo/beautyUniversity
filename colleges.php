748d4712f3d6fb52a61892f5705499093b311854
peter279k <peter279k@gmail.com>
2015-11-29 14:53:27 +0800

    add mb_strisr

--- colleges.php
+++ colleges.php
@@ -28,10 +28,12 @@
 		{
 			$message = str_replace("台", "臺", $data[$i]["message"]);
 			$message = str_replace("表特大學", "", $message);
-			/*
+
 			for($j=3$j<count($colleges);$j++)
 			{
 				$value = str_replace("國立", "", $colleges[$j]["name"]);
+				echo mb_stristr($message, "科技大學") . "<br>";
+				/*
 				if(mb_stristr($message, "科技大學") === true)
 				{
 					//if(mb_stristr($message, $value) === true)
@@ -42,7 +44,6 @@
 					//if(mb_strrpos($message, $value) === true)
 						$colleges[$j]["count"] += 1;
 				}
-				/*
 				else if(mb_stristr($message, "科大") === true)
 				{
 					if(mb_strrpos($message,$value) === true)
@@ -65,7 +66,6 @@
 
 				}
 			}
-			*/
 		}
 
 		echo json_encode($colleges);
