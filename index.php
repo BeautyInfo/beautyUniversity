972ca3b5274c3b6054e4e68bc4f9897ea459f01e
peter279k <peter279k@gmail.com>
2015-12-06 03:11:19 +0800

    remove variable $router from instance myController(no constructer)

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
 	
 	$router->get('/data_out/school/(\w+)', function($name) {
 		$req = htmlentities($name);
-		$controller = new myController($router);
+		$controller = new myController();
 		echo $controller -> indexAction("school_" . $req);
 	});
 	
 	$router -> get('/data_out/school/colleges/(\w+)', function($name) {
 		ob_start("ob_gzhandler");
 		$req = htmlentities($name);
-		$controller = new myController($router);
+		$controller = new myController();
 		echo $controller -> indexAction("colleges_" . $req);
 	});
 	
