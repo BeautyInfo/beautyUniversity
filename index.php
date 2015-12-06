b57fe47fb22058b49c0210fbd5ff5dc8a64ee283
peter279k <peter279k@gmail.com>
2015-12-06 14:56:11 +0800

    using http

--- index.php
+++ index.php
@@ -31,12 +31,11 @@
 		echo $controller -> indexAction("colleges_" . $req);
 	});
 	
-	/*
+	
 	$router->set404(function() {
 		header('HTTP/1.1 404 Not Found');
 		echo "invalid request url";
 	});
-	*/
 	
 	$router -> run();
 ?>
