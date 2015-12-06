9b64317f1556b02cd9a8a24ed324a489638edadf
peter279k <peter279k@gmail.com>
2015-12-06 13:23:02 +0800

    fix  set404 method

--- index.php
+++ index.php
@@ -34,8 +34,7 @@
 	$router->set404(function() {
 		header('HTTP/1.1 404 Not Found');
 		header('Content-Type: text/plain; charset=utf-8');
-		//echo "invalid request url";
-		echo $_SERVER["HTTPS"];
+		echo "invalid request url";
 	});
 	
 	$router -> run();
