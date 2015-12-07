1847fefdf8c48cbb2258c89d12d43b282ed66d12
peter279k <peter279k@gmail.com>
2015-12-07 15:35:52 +0800

    add csv convert to json file and analytic  fju result

--- model/libs/analytic.php
+++ model/libs/analytic.php
@@ -6,30 +6,14 @@
 		}
 		
 		private function parseFile($fileName) {
-			$handle = fopen($fileName, "r");
-			if(!$handle) {
-				return false;
+			if(!file_exists($fileName)) {
+				$handle = false;
 			}
 			else {
-				$colleges = array();
-				$count = 0;
-				
-				while (!feof($handle)) {
-					$arr = explode(",", fgets($handle));
-					if(isset($arr[1])) {
-						if($arr[1] !== "學校名稱" && $arr[1] !== "") {
-							$colleges[$count]["name"] = str_replace("台", "臺", $arr[1]);
-							$colleges[$count]["name"] = str_replace("國立", "", $colleges[$count]["name"]);
-							$colleges[$count]["count"] = 0;
-							$count += 1;
-					
-						}
-					}
-				}
-				
-				file_put_contents("files/u1_new.json", json_encode($colleges));
-				return $colleges;
+				$handle = file_get_contents($fileName);
 			}
+			
+			return $handle;
 		}
 		
 		private function httpGet($url) {
@@ -40,35 +24,45 @@
 		}
 		
 		private function processStr($colleges, $data) {
-			$result = 0;
-			$search = array("台", "表特大學", "文化大學", "美和大學", "臺北城市大學", "新生醫護管理學校", "美和護專",
-				"元培科技大學", "醒悟科技大學", "德明", "耕莘", "華夏大學", "臺北商業技術學院", "輔英大學");
-			$replace = array("臺", "", "中國文化大學", "美和科技大學", "臺北城市科技大學", "新生醫護管理專科學校",
-				"美和科技大學", "元培醫事科技大學", "醒吾科技大學", "德明財經科技大學", "耕莘健康管理專科學校", "華夏科技大學",
-				"國立臺北商業大學", "輔英科技大學");
-
-			for($i=0;$i<count($data);$i++) {
-				$message = trim($data[$i]["message"]);
-				$message = str_replace($search, $replace, $message);
+			if($this -> name === "university") {
+				$search = array("台", "表特大學", "文化大學", "美和大學", "臺北城市大學", "新生醫護管理學校", "美和護專",
+					"元培科技大學", "醒悟科技大學", "德明", "耕莘", "華夏大學", "臺北商業技術學院", "輔英大學");
+				$replace = array("臺", "", "中國文化大學", "美和科技大學", "臺北城市科技大學", "新生醫護管理專科學校",
+					"美和科技大學", "元培醫事科技大學", "醒吾科技大學", "德明財經科技大學", "耕莘健康管理專科學校", "華夏科技大學",
+					"國立臺北商業大學", "輔英科技大學");
+ 
+				for($i=0;$i<count($data);$i++) {
+					$message = trim($data[$i]["message"]);
+					$message = str_replace($search, $replace, $message);
 		
-				if(strpos($message, "科大") !== false) {
-					$message = str_replace("科大", "科技大學", $message);
-				}
-				if(strpos($message, "專校") !== false) {
-					$message = str_replace("專校", "專科學校", $message);
-				}
+					if(strpos($message, "科大") !== false) {
+						$message = str_replace("科大", "科技大學", $message);
+					}
+					if(strpos($message, "專校") !== false) {
+						$message = str_replace("專校", "專科學校", $message);
+					}
 			
-				$check = false;
-				$count = 0;
-				for($j=0;$j<count($colleges);$j++) {
-					//ignore china university
-					if(strpos($message, "中國傳媒大學") !== false)
-						continue;
-					if(strpos($message, $colleges[$j]["name"]) !== false) {
-						$colleges[$j]["count"] += 1;
-						$result += 1;
-						$check = true;
-						break;
+
+					$count = 0;
+					for($j=0;$j<count($colleges);$j++) {
+						//ignore china university
+						if(strpos($message, "中國傳媒大學") !== false)
+							continue;
+						if(strpos($message, $colleges[$j]["name"]) !== false) {
+							$colleges[$j]["count"] += 1;
+							break;
+						}
+					}
+				}
+			}
+			
+			if($this -> name === "FJU") {
+				for($i=0;$i<count($data);$i++) {
+					$message = trim($data[$i]["message"]);
+					for($j=0;$j<count($colleges);$j++) {
+						if(strpos($message, $colleges[$j]["name"]) !== false) {
+							$colleges[$j]["count"] += 1;
+						}
 					}
 				}
 			}
@@ -78,18 +72,24 @@
 		
 		public function getAnalyticRes() {
 			$fileName = "";
-			if($this -> name !== "FJU")
-				$fileName = "files/u1_new.csv";
-			else
-				$fileName = "files/fju.csv";
-
+			$url = "";
+			if($this -> name === "university") {
+				$fileName = "files/u1_new.json";
+				$url = "http://mywebservice.info/beautyUniversity/data_out/school/university";
+			}
+			
+			if($this -> name === "FJU") {
+				$fileName = "files/fju.json";
+				$url = "http://mywebservice.info/beautyUniversity/data_out/school/FJU";
+			}
+			
 			$handle = $this -> parseFile($fileName);
 			if($handle === false) {
-				return "cannot open csv file";
+				return "cannot open json file";
 			}
 			else {
-				$data = $this -> httpGet("http://mywebservice.info/beautyUniversity/data_out/school/university");
-				$colleges = $handle;
+				$data = $this -> httpGet($url);
+				$colleges = json_decode($handle, true);
 				$result = $this -> processStr($colleges, $data);
 				return json_encode($result);
 			}
