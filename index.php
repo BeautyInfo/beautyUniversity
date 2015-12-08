62c2c09e22fdd2422f342970f306b2996ea55e76
peter279k <peter279k@gmail.com>
2015-12-08 21:56:12 +0800

    修改api route url 寫法

--- index.php
+++ index.php
@@ -15,22 +15,16 @@
 		echo "Welcome to beautyUniversity JSON api";
 	});
 	
-	$router->get('/data_out/school/(\w+)', function($name) {
+	$router->get('/v1/school/(\w+)/analytic/(\w+)', function($name, $bool) {
 		header('Content-Type: application/json; charset=utf-8');
 		ob_start("ob_gzhandler");
 		$req = htmlentities($name);
 		$controller = new myController($req);
-		echo $controller -> indexAction("school_" . $req);
+		if($bool === false)
+			echo $controller -> indexAction("school_" . $req);
+		else
+			echo $controller -> indexAction("colleges_" . $req);
 	});
-	
-	$router -> get('/data_out/school/colleges/(\w+)', function($name) {
-		header('Content-Type: application/json; charset=utf-8');
-		ob_start("ob_gzhandler");
-		$req = htmlentities($name);
-		$controller = new myController($req);
-		echo $controller -> indexAction("colleges_" . $req);
-	});
-	
 	
 	$router->set404(function() {
 		header('HTTP/1.1 404 Not Found');
